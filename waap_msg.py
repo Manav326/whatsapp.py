afc2d90bc1b84dd8cd10792e3a66293987130bb2
Manavjeet Gupta <manaviitp123@gmail.com>
2019-01-31 21:38:00 +0530

    Update waap_msg.py

--- waap_msg.py
+++ waap_msg.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('http://web.whatsapp.com')
+driver.get('https://web.whatsapp.com/')
 
 name = input('Enter the name of user or group : ')
-msg = input('Enter the message : ')
+msg = input('Enter your message : ')
 count = int(input('Enter the count : '))
 
-#Scan the code before proceeding further
 input('Enter anything after scanning QR code')
 
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msg_box = driver.find_element_by_class_name('input-container')
+msg_box = driver.find_element_by_class_name('_2S1VP')
 
 for i in range(count):
     msg_box.send_keys(msg)
-    driver.find_element_by_class_name('compose-btn-send').click()
+    button = driver.find_element_by_class_name('_35EW6')
+    button.click()
